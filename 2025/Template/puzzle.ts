245db97042f364e3bbdda12ee8f20972142d3534
Anthony Reed <areed@bowdoin.edu>
2025-12-01 02:02:01 -0500

    Template update and day 2 peperation

---
+++ 2025/Template/puzzle.ts
@@ -0,0 +1,14 @@
+import { readFile } from "../Utilities/utilities"
+
+function part1(input: string[]): number {
+    // implentation for part 1
+    return 0;
+}
+
+function part2(input: string[]): number {
+    // implementation for part 2
+    return 0;
+}
+
+console.log(part1(readFile('input.txt')));
+console.log(part2(readFile('input.txt')));
